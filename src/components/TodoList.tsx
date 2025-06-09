e185e2f6379fed7887942e32eecd68abcca76f1d
Sarang Deodhar <Sarang.Deodhar@ril.com>
2025-06-09 19:09:23 +0530

    Second Commit

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,13 +9,25 @@ interface Props{
 }
 const TodoList: React.FC<Props> = ({todos,setTodos }: Props) => {
   return (
-    <div className='todos'>
-      {todos.map(todo => (
-        <SingleTodo todo = {todo} key = {todo.id}
-        todos = {todos}
-        setTodos = {setTodos}
-        />
-    ))}
+    <div className="container">
+      <div className="todos">
+      <span className="todos___heading">
+        Active Tasks
+      </span>
+      {
+        todos.map((todo)=> (
+          <SingleTodo 
+            todo={todo}
+            key={todo.id}
+            setTodos={setTodos} todos={todos}/>
+        ))
+      }
+      </div>
+      <div className="todos remove">
+      <span className="todos___heading">
+      Completed tasks
+      </span>
+      </div>
     </div>
   )
 }
